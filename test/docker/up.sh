b608e7f2f6a618471859c53506957257c65f2416
Jakob Erpf <58260820+jakoberpf@users.noreply.github.com>
2021-12-10 08:56:56 +0100

    prevent globbing and word splitting

--- test/docker/up.sh
+++ test/docker/up.sh
@@ -12,9 +12,9 @@ while [ $? -ne 0 ]; do
     echo "Waiting for Vault to be ready..."
 done
 
-vault operator unseal $UNSEAL_KEY_1
-vault operator unseal $UNSEAL_KEY_3
-vault operator unseal $UNSEAL_KEY_5
+vault operator unseal "$UNSEAL_KEY_1"
+vault operator unseal "$UNSEAL_KEY_3"
+vault operator unseal "$UNSEAL_KEY_5"
 
 vault_status=$(vault status | grep "Sealed" | tr -s ' ' | cut -d " " -f2)
 if [ "$vault_status" == "true" ]; then
